69ebf1da2209cfd9fd64588c66fdb045aeaf39cc
Avinash <53809765+avinash7375@users.noreply.github.com>
2025-03-17 08:53:23 +0530

    yesss

---
+++ pract5/pract5.js
@@ -0,0 +1,19 @@
+﻿<script>
+  angular.module('errorHandlingApp', [])
+    .controller('MainController', function($http) {
+      var vm = this;
+      vm.data = '';
+      vm.errorMessage = '';
+
+      vm.fetchData = function() {
+        $http.get('https://jsonplaceholder.typicode.com/invalid-url')
+          .then(function(response) {
+            vm.data = response.data;
+          })
+          .catch(function(error) {
+            vm.errorMessage = 'Failed to fetch data. Please try again later.';
+            console.error('Error:', error);
+          });
+      };
+    });
+</script>
